f2a20166fbfd3e7f0a2bc6a0e599289236b29bb1
unknown <muthuselvam422001@gmail.com>
2025-07-27 13:47:05 +0530

    setup correction

--- src/ConnectWidget.js
+++ src/ConnectWidget.js
@@ -2,12 +2,15 @@ import './css/App.css';
 
 function ConnectWidget() {
   return (
+  <div>
     <div className='Connect-navbar'>
         <div className='Connect-navbar-content'>
             <div className='Connect-navbar-icon'><img src="https://storagefiles.clo-set.com/public/connect/common/connect-desktop-header-bi.svg" alt="Connect Header Logo"></img></div>
             <div className='Connect-navbar-button'>Required Features</div>
         </div>
     </div>
+    
+  </div>
   );
 }
 
